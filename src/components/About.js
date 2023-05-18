1fc512d354e1702a9e607ea891fd296eb6081c8f
Kevin Bravo <kbravo1024@gmail.com>
2023-05-17 18:12:55 -0700

    fixed lab 2

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import { image } from "../data/data";
 function About() {
   return (
     <div id="about">
-      <h2>About me</h2>
+      <h2>About Me</h2>
       <p>Im so cool</p>
       <img src={image} alt="I made this"></img>
     </div>
